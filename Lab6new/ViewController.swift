80c02e693b1af919325d6688163ee27ecdf79a02
AZAD <mandariazad6666@gmail.com>
2024-02-19 19:40:24 -0800

    Initial Commit

---
+++ Lab6new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab6new
+//
+//  Created by user240113 on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
